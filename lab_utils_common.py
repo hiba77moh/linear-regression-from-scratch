a770b67e1422d2f00a9dc55f32958fee02f44b83
hiba77moh <hiba77moh@gmail.com>
2025-06-05 16:20:33 +0300

    setting up pre-written helper modules

---
+++ lab_utils_common.py
@@ -0,0 +1,13 @@
+""" 
+lab_utils_common.py
+    functions common to all optional labs, Course 1, Week 2 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('./deeplearning.mplstyle')
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0';
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
+
